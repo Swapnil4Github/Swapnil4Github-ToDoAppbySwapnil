a6af7e129248d6bc46ba8160934f29deade5887b
Swapnil Srivastava <62797602+Swapnil4Github@users.noreply.github.com>
2021-04-02 04:17:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, (err) => {
       else{
           console.log("Connected to db!");
       }
-app.listen(3000, () => console.log("Server Up and running"));
+app.listen(process.env.PORT || 3000, () => console.log("Server Up and running"));
 });
 
 
